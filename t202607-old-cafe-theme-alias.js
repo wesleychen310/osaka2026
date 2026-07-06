2117fecf65a4434d087aad9a4409ec563e460350
wesleychen310 <143617032+wesleychen310@users.noreply.github.com>
2026-07-07 00:05:49 +0800

    Alias old cafe tea theme id

---
+++ t202607-old-cafe-theme-alias.js
@@ -0,0 +1,4 @@
+if(window.KYOTO_THEMES&&window.KYOTO_THEMES.old_cafe_tea){
+ window.KYOTO_THEMES.old_coffee_tea={...window.KYOTO_THEMES.old_cafe_tea,id:'old_coffee_tea'};
+ delete window.KYOTO_THEMES.old_cafe_tea;
+}
