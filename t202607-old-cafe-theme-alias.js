7a7ed0636736efbac74c9cf8533b9f5c5ad274a6
wesleychen310 <143617032+wesleychen310@users.noreply.github.com>
2026-07-07 00:07:54 +0800

    Remove unused old cafe alias file

--- t202607-old-cafe-theme-alias.js
+++
@@ -1,4 +0,0 @@
-if(window.KYOTO_THEMES&&window.KYOTO_THEMES.old_cafe_tea){
- window.KYOTO_THEMES.old_coffee_tea={...window.KYOTO_THEMES.old_cafe_tea,id:'old_coffee_tea'};
- delete window.KYOTO_THEMES.old_cafe_tea;
-}
