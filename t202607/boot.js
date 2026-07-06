aa8b15a2a2b447c8686830fbd4074bdfe511e535
wesleychen310 <143617032+wesleychen310@users.noreply.github.com>
2026-07-06 23:09:42 +0800

    Add shared page boot loader

---
+++ t202607/boot.js
@@ -0,0 +1,7 @@
+const KYOTO_CACHE_VERSION='clean1';
+function loadScript(src){document.write('<script src="'+src+'?v='+KYOTO_CACHE_VERSION+'"><\/script>');}
+loadScript('../t202607-data.js');
+loadScript('../t202607-books-data.js');
+loadScript('../t202607-themes-data.js');
+loadScript('../t202607-places-data.js');
+loadScript('site.js');
