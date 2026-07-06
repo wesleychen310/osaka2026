154d86865e96be6623cbf314eabeb5cad64e50e7
wesleychen310 <143617032+wesleychen310@users.noreply.github.com>
2026-07-06 23:58:48 +0800

    Load old cafe tea theme data

--- t202607/boot.js
+++ t202607/boot.js
@@ -1,8 +1,9 @@
-const KYOTO_CACHE_VERSION='clean3';
+const KYOTO_CACHE_VERSION='clean4';
 function loadScript(src){document.write('<scr'+'ipt src="'+src+'?v='+KYOTO_CACHE_VERSION+'"></scr'+'ipt>');}
 loadScript('../t202607-data.js');
 loadScript('../t202607-books-data.js');
 loadScript('../t202607-themes-data.js');
+loadScript('../t202607-old-cafe-theme-data.js');
 loadScript('../t202607-architecture-data.js');
 loadScript('../t202607-places-data.js');
 loadScript('site.js');
