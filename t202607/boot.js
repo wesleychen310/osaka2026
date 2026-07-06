98a7b074f9ac5f8b0c3f865d01803ccf7d48523a
wesleychen310 <143617032+wesleychen310@users.noreply.github.com>
2026-07-06 23:30:33 +0800

    Bump cache and load architecture data

--- t202607/boot.js
+++ t202607/boot.js
@@ -1,7 +1,8 @@
-const KYOTO_CACHE_VERSION='clean2';
-function loadScript(src){document.write('<script src="'+src+'?v='+KYOTO_CACHE_VERSION+'"><\/script>');}
+const KYOTO_CACHE_VERSION='clean3';
+function loadScript(src){document.write('<scr'+'ipt src="'+src+'?v='+KYOTO_CACHE_VERSION+'"></scr'+'ipt>');}
 loadScript('../t202607-data.js');
 loadScript('../t202607-books-data.js');
 loadScript('../t202607-themes-data.js');
+loadScript('../t202607-architecture-data.js');
 loadScript('../t202607-places-data.js');
 loadScript('site.js');
