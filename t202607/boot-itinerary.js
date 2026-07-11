27a6e2d723c66119dbab655650dc262dd379d4f9
wesleychen310 <143617032+wesleychen310@users.noreply.github.com>
2026-07-12 05:10:37 +0800

    Refresh itinerary loader cache

--- t202607/boot-itinerary.js
+++ t202607/boot-itinerary.js
@@ -1,4 +1,4 @@
-const ITINERARY_CACHE_VERSION='d3-bal-isetan-20260711';
+const ITINERARY_CACHE_VERSION='d4-koyamaen-shoseien-nishiki-bal-20260712';
 const ITINERARY_FILES=[
  '../t202607-itinerary-route4-data.js',
  '../t202607-itinerary-weather-data.js',
